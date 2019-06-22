1d4a62b31565a8b71b4d82259b204b5e2ed8db8a
seono <tjsh0111@gmail.com>
2019-06-22 16:32:35 +0900

    dataserver sqlite3

--- dstagram/settings.py
+++ dstagram/settings.py
@@ -76,22 +76,22 @@ WSGI_APPLICATION = 'dstagram.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dstagram',
-        'USER': 'postgres',
-        'PASSWORD': '*s5827253',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dstagram',
+#         'USER': 'postgres',
+#         'PASSWORD': '*s5827253',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
