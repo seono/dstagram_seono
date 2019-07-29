205068f425cfa3376da1c8b5994a29873847238b
seono <tjsh0111@gmail.com>
2019-07-29 16:04:04 +0900

    heroku

--- dstagram/settings.py
+++ dstagram/settings.py
@@ -14,14 +14,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','v2)krtf8b^x$+(qe$$#jw00*fyj0((e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-<<<<<<< HEAD
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
-=======
-ALLOWED_HOSTS = [
-    'django-env.9jqzwia2md.us-west-2.elasticbeanstalk.com',
-    'localhost',
-    '127.0.0.1',]
->>>>>>> 0ee9f7ff1b4c50e52a013979346e077878d72d45
 
 
 # Application definition
@@ -73,22 +66,13 @@ WSGI_APPLICATION = 'dstagram.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aa1bp1g9ol6rnh2',
-        'USER': 'postgres',
-        'PASSWORD': '*s5827253',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 
 # Password validation
